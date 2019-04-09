9234b0fb7ad9f50537c0f0e41de11f2097240b95
asherLZR <asher.lzrz@gmail.com>
2019-04-09 18:18:44 +1000

    Add slash to dist

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app  = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res){
 	res.sendFile(__dirname + "/dist/index.html")
 });
